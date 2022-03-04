11906da7d6c6c921af829f6ae0f2dcddde755b1b
ville-petteri <ville-petteri.saily@live.com>
2022-03-04 10:49:01 +0200

    Oikean mittaisen listan luominen parametrin mukaan

--- vko6-2/tehtava2.py
+++ vko6-2/tehtava2.py
@@ -8,14 +8,15 @@ from azure.storage.blob import BlobServiceClient
 
 #parsitaan komentoriviargumentistä talteen Rivimäärä-muuttuja
 parser = argparse.ArgumentParser()
-parser.add_argument("Rivimäärä", type=int, help="Tulostettavien rivien määrä")
+parser.add_argument("Rivimaara", type=int, help="Tulostettavien rivien määrä")
 args = parser.parse_args()
 
 
 
 
 
-#ladataan tehtävässä 1 upattu tiedosto
+#ladataan tehtävässä 1 upattu tiedosto. 
+# Yhteys containeriin luodaan ympäristömuuttujassa olevan connection strigin avulla. Containerin ja tiedoston nimet annetaan parametreina
 def downloadfile(containername,filename):
     connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
     BlobServiceClient.from_connection_string(connection_string)
@@ -24,4 +25,20 @@ def downloadfile(containername,filename):
         blob_data = blob.download_blob()
         blob_data.readinto(my_blob)
 
-downloadfile("checkpointblob","checkpoint.txt")
+def CutSortAndPrint(filename):
+    readlist=[]
+    cutlist=[]
+    with open(filename) as file:
+        for rivi in file:
+            rivi=rivi.replace("\n","")
+            readlist.append(rivi)
+
+    cutlist=readlist[0:args.Rivimaara]
+    print(cutlist)
+
+#lajiteltu=sorted(alkulista,key=lambda x: (len(x),x))
+
+
+
+downloadfile("checkpointcontainer","checkpoint.txt")
+CutSortAndPrint("checkpoint.txt")
